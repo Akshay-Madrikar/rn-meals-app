ca31f26bbb8c54f7dca28f8dfbfc83a751853d6f
Akshay-Madrikar <akshaymadrikar@gmail.com>
2022-02-07 22:15:36 +0530

    basic navigations

---
+++ screens/CategoryMealsScreen.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+
+const CategoryMealsScreen = (props) => {
+  return (
+    <View style={styles.screen}>
+      <Text>The Category Meals Screen</Text>
+      <Button
+        title="Go to Meals"
+        onPress={() => {
+          props.navigation.navigate({ routeName: 'MealDetail' });
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignContent: 'center',
+  },
+});
+
+export default CategoryMealsScreen;
